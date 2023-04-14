76b5b80914919fc5f988e4973147f7c394d58f61
lcockerham <lcockerham@gmail.com>
2023-04-13 22:20:56 -0400

    add hit point calculations

--- Red_Wizard_Generator.py
+++ Red_Wizard_Generator.py
@@ -29,6 +29,31 @@ def calculate_hit_points(level, con_score):
 def calculate_modifier(score):
     return (score - 10) // 2
 
+def generate_ability_scores(level):
+    standard_array = [17, 14, 13, 12, 10, 8]
+    abilities = ["STR", "DEX", "CON", "WIS", "CHA"]
+
+    scores = {ability: 0 for ability in abilities}
+    scores["INT"] = standard_array.pop(0)
+
+    random.shuffle(standard_array)
+    for i, ability in enumerate(abilities):
+        scores[ability] = standard_array[i]
+
+    if level >= 8:
+        scores["INT"] = 20
+    elif level >= 4:
+        scores["INT"] = 18
+
+    return scores
+
+def generate_ability_modifiers(ability_scores):
+    modifiers = {}
+    for ability, score in ability_scores.items():
+        modifiers[f"{ability.lower()}_modifier"] = calculate_modifier(score)
+    return modifiers
+    
+
 def generate_thayan_name():
     first_name = random.choice(first_names)
     last_name = random.choice(last_names)
@@ -63,23 +88,7 @@ def generate_alignment():
     alignment_probabilities = [0.8] + [0.2 / (len(alignments) - 1)] * (len(alignments) - 1)
     return random.choices(alignments, weights=alignment_probabilities)[0]
 
-def generate_ability_scores(level):
-    standard_array = [17, 14, 13, 12, 10, 8]
-    abilities = ["STR", "DEX", "CON", "WIS", "CHA"]
 
-    scores = {ability: 0 for ability in abilities}
-    scores["INT"] = standard_array.pop(0)
-
-    random.shuffle(standard_array)
-    for i, ability in enumerate(abilities):
-        scores[ability] = standard_array[i]
-
-    if level >= 8:
-        scores["INT"] = 20
-    elif level >= 4:
-        scores["INT"] = 18
-
-    return scores
 
 
 def main(num_wizards, level=None):
@@ -98,12 +107,12 @@ def main(num_wizards, level=None):
             wizard["age"] = generate_age()
         wizard["alignment"] = generate_alignment()
         wizard["ability_scores"] = generate_ability_scores(wizard["level"])
-        dex_modifier = calculate_modifier(wizard["ability_scores"]["DEX"])
-        wizard["armor_class"] = 10 + dex_modifier
-        wizard["con_modifier"] = calculate_modifier(wizard["ability_scores"]["CON"])
+        wizard["ability_modifiers"] = generate_ability_modifiers(wizard["ability_scores"])
+        wizard["armor_class"] = 10 + wizard["ability_modifiers"]["dex_modifier"]
         wizard["hit_points"] = calculate_hit_points(wizard["level"], wizard["ability_scores"]["CON"])
         wizards.append(wizard)
-    
+
+   
     with open("red_wizards.json", "w") as outfile:
         json.dump(wizards, outfile, indent=2)
 

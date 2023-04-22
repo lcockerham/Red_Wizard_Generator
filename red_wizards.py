8810c710669d6c99d0a8c17cd2b36850bd87a333
lcockerham <lcockerham@gmail.com>
2023-04-22 12:33:58 -0400

    added ignore for json, fixed linting in red_wizards.py

--- red_wizards.py
+++ red_wizards.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
         print("Usage: python run_wizard_and_html.py <num_wizards> [level]")
         sys.exit(1)
 
-    num_wizards_arg = sys.argv[1]
-    level_arg = sys.argv[2] if len(sys.argv) >= 3 else None
+    NUM_WIZARDS_ARG = sys.argv[1]
+    LEVEL_ARG = sys.argv[2] if len(sys.argv) >= 3 else None
 
-    if level_arg:
-        generator_command = f"python red_wizard_generator.py {num_wizards_arg} {level_arg}"
+    if LEVEL_ARG:
+        generator_command = f"python red_wizard_generator.py {NUM_WIZARDS_ARG} {LEVEL_ARG}"
     else:
-        generator_command = f"python red_wizard_generator.py {num_wizards_arg}"
+        generator_command = f"python red_wizard_generator.py {NUM_WIZARDS_ARG}"
 
     print("Running Red_Wizard_Generator...")
     subprocess.run(generator_command, shell=True, check=True)

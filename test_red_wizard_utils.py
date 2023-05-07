293ef64550b4f1017600809e0a5895ec7f7acc15
lcockerham <lcockerham@gmail.com>
2023-05-07 14:16:29 -0400

    fixed linting, added challenge place holder and proficiency mod

--- test_red_wizard_utils.py
+++ test_red_wizard_utils.py
@@ -1,12 +1,12 @@
 """
 test_red_wizard_utils.py
 
-This module contains unit tests for the utility functions defined in the red_wizard_utils.py module.
-These tests aim to ensure the correct functioning of the functions used in generating Red Wizard characters
-for the Red Wizard Generator project.
+This module contains unit tests for the utility functions defined in the red_wizard_utils.py 
+module. These tests aim to ensure the correct functioning of the functions used in generating
+Red Wizard characters for the Red Wizard Generator project.
 
-The tests cover various aspects such as hit point calculations, ability score generation, and other
-character-specific traits.
+The tests cover various aspects such as hit point calculations, ability score generation, 
+and other character-specific traits.
 
 To run the tests, simply execute the following command in the terminal:
     python -m unittest test_red_wizard_utils
@@ -17,10 +17,11 @@ from red_wizards_utils import calculate_hit_points
 
 class TestCalculateHitPoints(unittest.TestCase):
     """
-    This class contains test cases for the calculate_hit_points function in the red_wizard_utils module.
+    This class contains test cases for the calculate_hit_points function in the 
+    red_wizard_utils module.
 
-    The test cases cover various scenarios involving different levels and Constitution scores to ensure
-    accurate hit point calculations for Red Wizard characters.
+    The test cases cover various scenarios involving different levels and Constitution scores 
+    to ensure accurate hit point calculations for Red Wizard characters.
 
     To run the tests in this class, simply execute the following command in the terminal:
         python -m unittest test_red_wizard_utils.TestCalculateHitPoints
@@ -28,7 +29,8 @@ class TestCalculateHitPoints(unittest.TestCase):
 
     def test_low_level_low_con(self):
         """
-        This method tests the calculate_hit_points function with a low level and low Constitution score.
+        This method tests the calculate_hit_points function with a low level and low Constitution
+        score.
         """
         level = 1
         con_score = 8
@@ -37,7 +39,8 @@ class TestCalculateHitPoints(unittest.TestCase):
 
     def test_low_level_high_con(self):
         """
-        This method tests the calculate_hit_points function with a low level and high Constitution score.
+        This method tests the calculate_hit_points function with a low level and high Constitution
+        score.
         """
         level = 1
         con_score = 18
@@ -46,7 +49,8 @@ class TestCalculateHitPoints(unittest.TestCase):
 
     def test_high_level_low_con(self):
         """
-        This method tests the calculate_hit_points function with a high level and low Constitution score.
+        This method tests the calculate_hit_points function with a high level and low Constitution
+        score.
         """
         level = 20
         con_score = 8
@@ -55,7 +59,8 @@ class TestCalculateHitPoints(unittest.TestCase):
 
     def test_high_level_high_con(self):
         """
-        This method tests the calculate_hit_points function with a high level and high Constitution score.
+        This method tests the calculate_hit_points function with a high level and high Constitution
+        score.
         """
         level = 20
         con_score = 18
